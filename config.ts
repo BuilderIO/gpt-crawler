632a82df428ca87c9b96aef80b23f12fbe9673cc
guillermoscript <arepayquezo@gmail.com>
2023-11-28 10:05:10 -0400

    Remove maxFileSize and maxTokens from defaultConfig, this are optional

--- config.ts
+++ config.ts
@@ -5,6 +5,4 @@ export const defaultConfig: Config = {
   match: "https://www.builder.io/c/docs/**",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
-  maxFileSize: 1000,
-  maxTokens: 5000
 };
