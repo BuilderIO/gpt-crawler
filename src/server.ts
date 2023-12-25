8e15bb3b426101cc9832ee82f63bc9a19630e026
Aditya Karnam <akarnam37@gmail.com>
2023-12-24 18:34:02 -0600

    fix: run prettier

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,45 @@
-import express from 'express';
-import cors from 'cors';
-import { readFile } from 'fs/promises';
+import express from "express";
+import cors from "cors";
+import { readFile } from "fs/promises";
 import { crawl, write } from "./core.js";
-import { Config, configSchema } from './config.js';
-import { configDotenv } from 'dotenv';
-import swaggerUi from 'swagger-ui-express';
+import { Config, configSchema } from "./config.js";
+import { configDotenv } from "dotenv";
+import swaggerUi from "swagger-ui-express";
 // @ts-ignore
-import swaggerDocument from '../swagger-output.json' assert { type: 'json' };
+import swaggerDocument from "../swagger-output.json" assert { type: "json" };
 
 configDotenv();
 
 const app = express();
 const port = Number(process.env.API_PORT) || 3000;
-const hostname = process.env.API_HOST || 'localhost';
+const hostname = process.env.API_HOST || "localhost";
 
 app.use(cors());
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Define a POST route to accept config and run the crawler
-app.post('/crawl', async (req, res) => {
-    const config: Config = req.body;
-    try {
-        const validatedConfig = configSchema.parse(config);
-        await crawl(validatedConfig);
-        await write(validatedConfig);
-        const outputFileContent = await readFile(validatedConfig.outputFileName, 'utf-8');
-        res.contentType('application/json');
-        return res.send(outputFileContent);
-    } catch (error) {
-        return res.status(500).json({ message: 'Error occurred during crawling', error });
-    }
+app.post("/crawl", async (req, res) => {
+  const config: Config = req.body;
+  try {
+    const validatedConfig = configSchema.parse(config);
+    await crawl(validatedConfig);
+    await write(validatedConfig);
+    const outputFileContent = await readFile(
+      validatedConfig.outputFileName,
+      "utf-8",
+    );
+    res.contentType("application/json");
+    return res.send(outputFileContent);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error occurred during crawling", error });
+  }
 });
 
 app.listen(port, hostname, () => {
-    console.log(`API server listening at http://${hostname}:${port}`);
+  console.log(`API server listening at http://${hostname}:${port}`);
 });
 
 export default app;
